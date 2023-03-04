f4eec24b19e324bf1aa7d475098993e8a5b6c735
Benjamin Wootton <55540500+benjaminwootton@users.noreply.github.com>
2023-03-04 11:00:59 +0000

    Update payments_by_type.sql

--- models/payments_by_type.sql
+++ models/payments_by_type.sql
@@ -1,4 +1,4 @@
 
 -- Use the `ref` function to select from other models
 
-select payment_type, count(*) as cnt, 'constant' as constant_currency from default.trips group by payment_type
+select payment_type, count(*) as cnt, 'JPY' as constant_currency from default.trips group by payment_type
