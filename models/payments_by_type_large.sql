125744e0e229a76b2149b3c8deaa751a7d931264
Benjamin Wootton <benjamin.wootton.personal+github@gmail.com>
2023-03-11 09:30:44 +0000

    new breaking test

--- models/payments_by_type_large.sql
+++ models/payments_by_type_large.sql
@@ -1,4 +1,4 @@
 
 -- Use the `ref` function to select from other models
 
-select payment_type, count(*) as cnt, 'JPY' as constant_currency from default.trips where fare_amount > 100 group by payment_type
+select payment_type, count(*) as cnt, null as constant_currency from default.trips where fare_amount > 100 group by payment_type
